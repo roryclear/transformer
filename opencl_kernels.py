30eedd6c9f532a882ea2b4abe33b8816851a2190
unknown <roryclear.rc@gmail.com>
2024-03-25 00:59:38 +0800

    forgot to add file

--- opencl_kernels.py
+++ opencl_kernels.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pyopencl as cl
+import time
 
 platform = cl.get_platforms()
 my_gpu_devices = platform[0].get_devices(device_type=cl.device_type.GPU)
@@ -8,29 +9,36 @@ queue = cl.CommandQueue(ctx)
 mf = cl.mem_flags
 
 def add(a,b):
-  dim = len(a)
-  a = a.flatten()
-  b = b.flatten()
-  a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a)
-  b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b)
+    a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a)
+    res_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b)
 
-  res_np = np.empty(np.shape(a)).astype(np.float32).flatten()
-  res_g = cl.Buffer(ctx, mf.WRITE_ONLY, (dim * 4))
+    res_np = np.zeros(np.shape(a)).astype(np.float32).flatten()
+    #res_g = cl.Buffer(ctx, mf.WRITE_ONLY, (dim * 4))
 
-  prg = cl.Program(ctx, f"""
-  __kernel void add(
-      __global const float *a, __global float *res)
-  {{
-  }}
-  """).build()
-  knl = prg.add
-  knl(queue, (1,1), (1,1), a_g, res_g) #todo check shape
-  cl.enqueue_copy(queue, res_np, res_g)
-  return res_np
+    prg = cl.Program(ctx, f"""
+    __kernel void add(
+        __global const float *a, __global float *res)
+    {{
+    int lidx0 = get_local_id(0);
+    int gidx0 = get_global_id(0);
+        res[gidx0] += a[gidx0];   
+    }}
+    """).build()
+    knl = prg.add
+    knl(queue, (784,1), (1,1), a_g, res_g) #todo check shape
+    cl.enqueue_copy(queue, res_np, res_g)
+    return res_np
 
-a = np.zeros([1,1,784])
-b = np.zeros([1,1,784])
-a.fill(4)
-b.fill(6)
-c = add(a,b)
-print(c)
+a = np.random.rand(1,1,784).astype(np.float32)
+b = np.random.rand(1,1,784).astype(np.float32)
+
+st = time.perf_counter()
+for i in range(1):
+    c = add(a,b)
+clt = time.perf_counter() - st
+st = time.perf_counter()
+for i in range(100):
+    c_np = a+b
+npt = time.perf_counter() - st
+np.testing.assert_allclose(c,(a+b).flatten(),rtol=0.001)
+print("passed",clt,npt)
