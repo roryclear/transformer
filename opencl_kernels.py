5bfaf200c2df52bd98fea84039cc295aaea5903d
unknown <roryclear.rc@gmail.com>
2024-03-30 23:17:46 +0800

    use multithreaded avg, same speed but might make fusing easier

--- opencl_kernels.py
+++ opencl_kernels.py
@@ -29,14 +29,14 @@ def add(a,b,b_s=0,a_s=0):
     cl.enqueue_copy(queue, res_np, res_g)
     return res_np
 
-len = 768000
+len = 768
 loop_size = int(len / 256)
 len_short = 768
-def minus_mean_large(a,b):
+def minus_mean_multi(a):
     a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a)
     prg = cl.Program(ctx, f"""
     __kernel void sum(
-        __global float *data0, const __global float *data1)
+        __global float *data0)
     {{
     float avg = 0;
     __attribute__ ((aligned (16))) __local float temp[256];
@@ -44,7 +44,7 @@ def minus_mean_large(a,b):
     int gidx0 = get_global_id(0); /* 256 */
     float acc0 = 0.0f;
     for (int ridx0 = 0; ridx0 < {loop_size}; ridx0++) {{
-        float val0 = data1[(lidx0*{loop_size})+ridx0];
+        float val0 = data0[(lidx0*{loop_size})+ridx0];
         acc0 = (val0+acc0);
     }}
     temp[lidx0] = acc0;
@@ -62,9 +62,7 @@ def minus_mean_large(a,b):
     }}
     """).build()
     knl = prg.sum
-
-    res_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b)
-    knl(queue, (256,1), (256,1), a_g, res_g) #rory to test large stuff
+    knl(queue, (256,1), (256,1), a_g) #rory to test large stuff
     cl.enqueue_copy(queue, a, a_g)
     return a
 
@@ -91,43 +89,17 @@ def minus_mean(a):
 '''
 a = np.random.rand(len).astype(np.float32)
 b = np.copy(a)
-c = minus_mean_large(a,a)
+st = time.perf_counter()
+for i in range(10000):
+    c = minus_mean(a)
+t1 = time.perf_counter() - st
+st = time.perf_counter()
+for i in range(10000):
+    c2 = minus_mean_multi(a)
+t2 = time.perf_counter() - st
+print("t1 =",t1)
+print("t2 =",t2)
 cnp = b - np.mean(b)
 np.testing.assert_allclose(c,cnp,atol=1e-6)
-a = np.random.rand(len).astype(np.float32)
-b = np.random.rand(len).astype(np.float32)
-
-st = time.perf_counter()
-for i in range(100):
-    c_m = minus_mean(a,b) #same but multi
-t = time.perf_counter() - st
-print("rory time taken multi =",t)
-
-st = time.perf_counter()
-for i in range(100):
-    c = sum(a,b)
-t = time.perf_counter() - st
-print("rory time taken =",t)
-
-c_np = a - np.mean(a)
-#print("rory a and c",c,c_np)
-np.testing.assert_allclose(c,c_np,atol=1e-4)
-np.testing.assert_allclose(c,c_m,atol=1e-4)
-minus mean stuff
-
-a = np.random.rand(1,1,768).astype(np.float32)
-b = np.random.rand(1,1,768).astype(np.float32)
-
-st = time.perf_counter()
-for i in range(1000):
-    c = add(a,b)
-clt = time.perf_counter() - st
-c = c.reshape(1,1,768)
-st = time.perf_counter()
-for i in range(100000):
-    c_np = a+b
-npt = time.perf_counter() - st
-print("rory shapes =",np.shape(c),np.shape(a+b))
-np.testing.assert_allclose(c,(a+b),rtol=0.001)
-print("passed",clt,npt)
+np.testing.assert_allclose(c2,cnp,atol=1e-6)
 '''
