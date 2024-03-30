88bc31f34c2fcd31a0f1ea50f39dad124f9ba7e3
unknown <roryclear.rc@gmail.com>
2024-03-30 22:29:56 +0800

    mess start trying barriers

--- opencl_kernels.py
+++ opencl_kernels.py
@@ -29,29 +29,44 @@ def add(a,b,b_s=0,a_s=0):
     cl.enqueue_copy(queue, res_np, res_g)
     return res_np
 
-len = 768
+len = 768000
+loop_size = int(len / 256)
+len_short = 768
 def sum(a,b):
     a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a)
     prg = cl.Program(ctx, f"""
     __kernel void sum(
-        __global float *a, __global float *res)
+        __global float *data0, const __global float *data1)
     {{
     float avg = 0;
-    for(int i = 0; i < {len}; i++) {{
-        avg += a[i];
+    __attribute__ ((aligned (16))) __local float temp[256];
+    int lidx0 = get_local_id(0); /* 256 */
+    int gidx0 = get_global_id(0); /* 256 */
+    float acc0 = 0.0f;
+    for (int ridx0 = 0; ridx0 < {loop_size}; ridx0++) {{
+        float val0 = data1[(lidx0*{loop_size})+ridx0];
+        acc0 = (val0+acc0);
     }}
-    avg = avg / {len};
-    for(int i = 0; i < {len}; i++) {{
-        res[i] = a[i] - avg;
+    temp[lidx0] = acc0;
+    barrier(CLK_LOCAL_MEM_FENCE); //rory need this barrier when input is large like 7680, 768 seems to work
+    //I think this woudlnt work if loop was smaller than len / 256?
+    float acc1 = 0.0f;
+    for (int ridx1 = 0; ridx1 < 256; ridx1++) {{
+        float val1 = temp[ridx1];
+        acc1 += val1;
+    }}
+    avg = acc1 / {len};
+    for(int i = 0; i < {loop_size}; i++) {{
+        data0[lidx0*{loop_size} + i] = data0[lidx0*{loop_size} + i] - avg;
     }}
     }}
     """).build()
     knl = prg.sum
 
     res_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b)
-    knl(queue, (1,1), (1,1), a_g, res_g)
-    cl.enqueue_copy(queue, b, res_g)
-    return b
+    knl(queue, (256,1), (256,1), a_g, res_g) #rory to test large stuff
+    cl.enqueue_copy(queue, a, a_g)
+    return a
 
 def minus_mean(a,b):
     a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a)
@@ -60,10 +75,10 @@ def minus_mean(a,b):
         __global float *a, __global float *res)
     {{
     float avg = 0;
-    for(int i = 0; i < {len}; i++) {{
+    for(int i = 0; i < {len_short}; i++) {{
         avg += a[i];
     }}
-    avg = avg / {len};
+    avg = avg / {len_short};
     int gidx0 = get_global_id(0);
     res[gidx0] = a[gidx0] - avg;
     }}
@@ -74,8 +89,13 @@ def minus_mean(a,b):
     knl(queue, (len,1), (256,1), a_g, res_g) #has to be multiple of 256
     cl.enqueue_copy(queue, b, res_g)
     return b
-
 '''
+a = np.random.rand(len).astype(np.float32)
+b = np.copy(a)
+c = sum(a,a)
+cnp = b - np.mean(b)
+np.testing.assert_allclose(c,cnp,atol=1e-6)
+
 a = np.random.rand(len).astype(np.float32)
 b = np.random.rand(len).astype(np.float32)
 
