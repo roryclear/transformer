c80e826798194c01020acb9d859aa0fa8beb5c54
unknown <roryclear.rc@gmail.com>
2024-04-09 21:55:36 +0800

    faster matvec than tg maybe

--- matmul_opencl.py
+++ matmul_opencl.py
@@ -40,7 +40,7 @@ def matmul(a,b):
 def matmul2(a,b):
     a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a)
     b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b)
-    c = np.zeros([1,4])
+    c = np.zeros([1,128])
     c = np.float32(c)
     c_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=c)
 
@@ -51,38 +51,38 @@ def matmul2(a,b):
         __global const float *a, __global const float *b, __global float *res)
     {{
     for(int y = 0; y < 10000; y++) {{
-        int x = get_local_id(0) + get_group_id(0) * 16;
-        float acc0 = 0.0f;
-        
-        float4 vals_a[16];
-        for(int i = 0; i < 16; i++) {{
-            vals_a[i] = (float4)(*((__global float4*)(a+i*4)));
-        }}
-                     
-        float vals_b[64];
-        for(int i = 0; i < 64; i++) {{
-            vals_b[i] = b[x+i*4];
-        }}
-        
-        for(int i = 0; i < 16; i++) {{
-            acc0 = mad((vals_a[i]).x,vals_b[i*4],acc0);
-            acc0 = mad((vals_a[i]).y,vals_b[i*4 + 1],acc0);
-            acc0 = mad((vals_a[i]).z,vals_b[i*4 + 2],acc0);
-            acc0 = mad((vals_a[i]).w,vals_b[i*4 + 3],acc0);
-        }}
-        res[x] = acc0;
+    int x = get_global_id(0);
+    float acc0 = 0.0f;
+    
+    float4 vals_a[16];
+    for(int i = 0; i < 16; i++) {{
+        vals_a[i] = (float4)(*((__global float4*)(a+i*4)));
+    }}
+                    
+    float vals_b[64];
+    for(int i = 0; i < 64; i++) {{
+        vals_b[i] = b[x+i*128]; // rory i*x
+    }}
+    
+    for(int i = 0; i < 16; i++) {{
+        acc0 = mad((vals_a[i]).x,vals_b[i*4],acc0);
+        acc0 = mad((vals_a[i]).y,vals_b[i*4 + 1],acc0);
+        acc0 = mad((vals_a[i]).z,vals_b[i*4 + 2],acc0);
+        acc0 = mad((vals_a[i]).w,vals_b[i*4 + 3],acc0);
+    }}
+    res[x] = acc0;
     }}
     }}
     """).build()
     knl = prg.matmul
-    knl(queue, (64,1), (64,1), a_g, b_g,c_g) #todo check shape
+    knl(queue, (128,1), (128,1), a_g, b_g,c_g) #shape of output !!!!!
     cl.enqueue_copy(queue, c, c_g)
     return c
 
 def matmul2_tg(a,b):
     a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a)
     b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b)
-    c = np.zeros([1,4])
+    c = np.zeros([1,128])
     c = np.float32(c)
     c_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=c)
 
@@ -92,29 +92,50 @@ def matmul2_tg(a,b):
     __kernel void matmul(
         __global const float *data1, __global const float *data2, __global float *data0)
     {{
-    __attribute__ ((aligned (16))) __local float temp[16];
-    int gidx0 = get_group_id(0); /* 4 */
-    int lidx1 = get_local_id(0); /* 16 */
+    __attribute__ ((aligned (16))) __local float temp[128];
+    for(int y = 0; y < 10000; y++) {{
+    int gidx0 = get_group_id(0); /* 8 */
+    int lidx1 = get_local_id(1); /* 4 */
+    int lidx2 = get_local_id(0); /* 8 */
     float acc0 = 0.0f;
-    for (int ridx0 = 0; ridx0 < 4; ridx0++) {{
-        float val0 = data1[(lidx1*4)+ridx0];
-        float val1 = data2[gidx0+(lidx1*16)+(ridx0*4)];
+    float acc1 = 0.0f;
+    float acc2 = 0.0f;
+    float acc3 = 0.0f;
+    int alu0 = ((gidx0*16)+lidx1);
+    int alu1 = (lidx1*32);
+    for (int ridx0 = 0; ridx0 < 8; ridx0++) {{
+        float val0 = data1[lidx2+(ridx0*8)];
+        int alu2 = (alu0+(lidx2*128)+(ridx0*1024));
+        float val1 = data2[alu2];
+        float val2 = data2[alu2+4];
+        float val3 = data2[alu2+8];
+        float val4 = data2[alu2+12];
         acc0 = mad(val0,val1,acc0);
+        acc1 = mad(val0,val2,acc1);
+        acc2 = mad(val0,val3,acc2);
+        acc3 = mad(val0,val4,acc3);
     }}
-    temp[lidx1] = acc0;
+    *((__local float4*)(temp+alu1+(lidx2*4))) = (float4)(float4)(acc0,acc1,acc2,acc3);
     barrier(CLK_LOCAL_MEM_FENCE);
-    if ((lidx1<1)) {{
-        float acc1 = 0.0f;
-        for (int ridx1 = 0; ridx1 < 16; ridx1++) {{
-        float val2 = temp[ridx1];
-        acc1 = (val2+acc1);
+    if ((lidx2<1)) {{
+        float4 acc4 = (float4)(0.0f,0.0f,0.0f,0.0f);
+        for (int ridx1 = 0; ridx1 < 8; ridx1++) {{
+        float4 val5 = (float4)(*((__local float4*)(temp+alu1+(ridx1*4))));
+        (acc4).x = ((val5).x+(acc4).x);
+        (acc4).y = ((val5).y+(acc4).y);
+        (acc4).z = ((val5).z+(acc4).z);
+        (acc4).w = ((val5).w+(acc4).w);
         }}
-        data0[gidx0] = acc1;
+        data0[alu0] = (acc4).x;
+        data0[alu0+4] = (acc4).y;
+        data0[alu0+8] = (acc4).z;
+        data0[alu0+12] = (acc4).w;
+    }}
     }}
     }}
     """).build()
     knl = prg.matmul
-    knl(queue, (64,1), (16,1), a_g, b_g,c_g) #todo check shape
+    knl(queue, (64,4), (8,4), a_g, b_g,c_g) #todo check shape 16*x and 16*1 (DONT CHANGE)
     cl.enqueue_copy(queue, c, c_g)
     return c
 
@@ -133,15 +154,14 @@ def time_it(func,a,b,s,i=1):
 
 ####SMALL MATMUL...MAKE BIGGER###
 a = np.random.rand(1,64).astype(np.float32)
-b = np.random.rand(64,4).astype(np.float32)
+b = np.random.rand(64,128).astype(np.float32)
 c_np = np.matmul(a,b)
 c2 = time_it(matmul2_tg,a,b,"tg",100)
 c = time_it(matmul2,a,b,"mine",100)
-print(c,c_np)
 np.testing.assert_allclose(c,c_np,rtol=1e-5)
 np.testing.assert_allclose(c2,c_np,rtol=1e-5)
 
-
+'''
 a = np.random.rand(1,64).astype(np.float32)
 b = np.random.rand(64,112).astype(np.float32)
 #a.fill(1)
@@ -156,4 +176,5 @@ print("time taken:",t)
 
 c_np = np.matmul(a,b)
 np.testing.assert_allclose(c,c_np,rtol=1e-6)
+'''
 
