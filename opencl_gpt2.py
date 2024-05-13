83f33fdf196e634cd705797c2cf803ca9278c679
unknown <roryclear.rc@gmail.com>
2024-05-13 21:26:18 +0800

    start simpler matmul kernel

--- opencl_gpt2.py
+++ opencl_gpt2.py
@@ -259,6 +259,7 @@ class Transformer:
         values[start_pos] = xv
         keys = keys.transpose(1,2,0) #todo, can we not do this?
         xq = openclk.matmul2(xq,keys,np.shape(keys)[2])
+        #xq = openclk.matmul2(xq,keys,np.shape(keys)[2])
         xq = openclk.minus_max(xq,(start_pos+1))
         values = values.transpose(1,0,2)
         xq = openclk.matmul3(xq,values,(start_pos+1))
--- opencl_kernels.py
+++ opencl_kernels.py
@@ -399,6 +399,37 @@ def matmul2(a,b,s=112):
     cl.enqueue_copy(queue, c, c_g)
     return c
 
+def matmul2_b(a,b):
+    s = np.shape(b)[2]
+    a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a)
+    b = b.flatten() #todo, shouldnt be needed
+    b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b)
+    c = np.zeros([12,1,s])
+    c = np.float32(c)
+    c_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=c)
+
+    #res_g = cl.Buffer(ctx, mf.WRITE_ONLY, (dim * 4))
+
+    prg = cl.Program(ctx, f"""
+    __kernel void matmul(
+        __global const float *a, __global const float *b, __global float *res)
+    {{
+    for(int k = 0; k < {12*s}; k++) {{
+        for(int x = 0; x < {s}; x++) {{
+            float acc0 = 0.0f;
+            for(int i = 0; i < 64; i++) {{
+                acc0 += a[i + 64*k] * b[x+i*{s} + {s}*64*k];
+            }}                  
+            res[x + k*{s}] = acc0 / 8; //hardcoded math.sqrt(self.head_dim)
+        }}
+    }}
+    }}
+    """).build()
+    knl = prg.matmul
+    knl(queue, (1,1), (1,1), a_g, b_g,c_g)
+    cl.enqueue_copy(queue, c, c_g)
+    return c
+
 def minus_max(a,s=112):
     a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a)
     c = np.zeros((12,1,s))
@@ -764,4 +795,4 @@ def time_it(func,a,b,i=100):
         total_time += t
         if f is None or t < f:
             f = t
-    return ret,f
+    return ret,f 
