de361a548ce211df424045b5536a31c1c0b00897
unknown <roryclear.rc@gmail.com>
2024-07-07 19:49:36 +0100

    remove awful loops

--- medium/model_loader.py
+++ medium/model_loader.py
@@ -153,137 +153,90 @@ print(type(model.transformer.wte.weight))
 print(model.transformer.wte.weight.size())
 print(model.transformer.wte.weight[0][0].item())
 
+#x = model.transformer.wpe.weight.detach().cpu().numpy()
+#print(x)
+#print(type(x))
+#print(type(x[0][0]))
 
 print("converting wpe_weight")
-gpt2_blank.model.wpe_weight = np.zeros(model.transformer.wpe.weight.size()).astype(np.float32)
-convert_2d(model.transformer.wpe.weight,gpt2_blank.model.wpe_weight)
-np.testing.assert_allclose(gpt2_blank.model.wpe_weight,gpt2.model.wpe.weight)
-print(np.isfortran(gpt2_blank.model.wpe_weight),np.isfortran(gpt2.model.wpe.weight))
+gpt2_blank.model.wpe_weight = model.transformer.wpe.weight.detach().cpu().numpy().astype(np.float32)
 
 print("converting ln_f.weight")
-gpt2_blank.model.ln_f_weight = np.zeros(model.transformer.ln_f.weight.size()).astype(np.float32)
-convert_1d(model.transformer.ln_f.weight,gpt2_blank.model.ln_f_weight)
-np.testing.assert_allclose(gpt2_blank.model.ln_f_weight,gpt2.model.ln_f.weight)
-print(np.isfortran(gpt2_blank.model.ln_f_weight),np.isfortran(gpt2.model.ln_f.weight))
+gpt2_blank.model.ln_f_weight = model.transformer.ln_f.weight.detach().cpu().numpy().astype(np.float32)
 
 print("converting ln_f.bias")
-gpt2_blank.model.ln_f_bias = np.zeros(model.transformer.ln_f.bias.size()).astype(np.float32)
-convert_1d(model.transformer.ln_f.bias,gpt2_blank.model.ln_f_bias)
-np.testing.assert_allclose(gpt2_blank.model.ln_f_bias,gpt2.model.ln_f.bias)
-print(np.isfortran(gpt2_blank.model.ln_f_bias),np.isfortran(gpt2.model.ln_f.bias))
+gpt2_blank.model.ln_f_bias = model.transformer.ln_f.bias.detach().cpu().numpy().astype(np.float32)
 
 print("converting mlp_c_proj.bias")
 gpt2_blank.model.mlp_c_proj_bias = []
 for x in range(12):
-    gpt2_blank.model.mlp_c_proj_bias.append(np.zeros(model.transformer.h[x].mlp.c_proj.bias.size()).astype(np.float32))
-    convert_1d(model.transformer.h[x].mlp.c_proj.bias,gpt2_blank.model.mlp_c_proj_bias[x])
-    np.testing.assert_allclose(gpt2_blank.model.mlp_c_proj_bias[x],gpt2.model.h[x].mlp.c_proj.bias)  
-print(np.isfortran(gpt2_blank.model.mlp_c_proj_bias[0]),np.isfortran(gpt2.model.h[0].mlp.c_proj.bias))
+    gpt2_blank.model.mlp_c_proj_bias.append(model.transformer.h[x].mlp.c_proj.bias.detach().cpu().numpy().astype(np.float32))
 
 print("converting mlp_c_proj.weight")
 gpt2_blank.model.mlp_c_proj_weight = []
 for x in range(12):
-    gpt2_blank.model.mlp_c_proj_weight.append(np.zeros(model.transformer.h[x].mlp.c_proj.weight.size()).astype(np.float32))
-    convert_2d(model.transformer.h[x].mlp.c_proj.weight,gpt2_blank.model.mlp_c_proj_weight[x])
-    np.testing.assert_allclose(gpt2_blank.model.mlp_c_proj_weight[x],gpt2.model.h[x].mlp.c_proj.weight)
-print(np.isfortran(gpt2_blank.model.mlp_c_proj_weight[0]),np.isfortran(gpt2.model.h[0].mlp.c_proj.weight))  
+    gpt2_blank.model.mlp_c_proj_weight.append(model.transformer.h[x].mlp.c_proj.weight.detach().cpu().numpy().astype(np.float32))
+
 
 print("converting mlp_c_fc.bias")
 gpt2_blank.model.mlp_c_fc_bias = []
 for x in range(12):
-    gpt2_blank.model.mlp_c_fc_bias.append(np.zeros(model.transformer.h[x].mlp.c_fc.bias.size()).astype(np.float32))
-    convert_1d(model.transformer.h[x].mlp.c_fc.bias,gpt2_blank.model.mlp_c_fc_bias[x])
-    np.testing.assert_allclose(gpt2_blank.model.mlp_c_fc_bias[x],gpt2.model.h[x].mlp.c_fc.bias)
-print(np.isfortran(gpt2_blank.model.mlp_c_fc_bias[0]),np.isfortran(gpt2.model.h[0].mlp.c_fc.bias))  
+    gpt2_blank.model.mlp_c_fc_bias.append(model.transformer.h[x].mlp.c_fc.bias.detach().cpu().numpy().astype(np.float32))
 
 print("converting mlp_c_fc.weight")
 gpt2_blank.model.mlp_c_fc_weight = []
 for x in range(12):
-    gpt2_blank.model.mlp_c_fc_weight.append(np.zeros(model.transformer.h[x].mlp.c_fc.weight.size()).astype(np.float32))
-    convert_2d(model.transformer.h[x].mlp.c_fc.weight,gpt2_blank.model.mlp_c_fc_weight[x])
-    np.testing.assert_allclose(gpt2_blank.model.mlp_c_fc_weight[x],gpt2.model.h[x].mlp.c_fc.weight)
-print(np.isfortran(gpt2_blank.model.mlp_c_fc_weight[0]),np.isfortran(gpt2.model.h[0].mlp.c_fc.weight))  
+    gpt2_blank.model.mlp_c_fc_weight.append(model.transformer.h[x].mlp.c_fc.weight.detach().cpu().numpy().astype(np.float32))
 
 print("converting attn_c_proj.bias")
 gpt2_blank.model.attn_c_proj_bias = []
 for x in range(12):
-    gpt2_blank.model.attn_c_proj_bias.append(np.zeros(model.transformer.h[x].attn.c_proj.bias.size()).astype(np.float32))
-    convert_1d(model.transformer.h[x].attn.c_proj.bias,gpt2_blank.model.attn_c_proj_bias[x])
-    np.testing.assert_allclose(gpt2_blank.model.attn_c_proj_bias[x],gpt2.model.h[x].attn.c_proj.bias)
-print(np.isfortran(gpt2_blank.model.attn_c_proj_bias[0]),np.isfortran(gpt2.model.h[0].attn.c_proj.bias))  
+    gpt2_blank.model.attn_c_proj_bias.append(model.transformer.h[x].attn.c_proj.bias.detach().cpu().numpy().astype(np.float32))
 
 print("converting attn_c_proj.weight")
 gpt2_blank.model.attn_c_proj_weight = []
 for x in range(12):
-    gpt2_blank.model.attn_c_proj_weight.append(np.zeros(model.transformer.h[x].attn.c_proj.weight.size()).astype(np.float32))
-    convert_2d(model.transformer.h[x].attn.c_proj.weight,gpt2_blank.model.attn_c_proj_weight[x])
-    np.testing.assert_allclose(gpt2_blank.model.attn_c_proj_weight[x],gpt2.model.h[x].attn.c_proj.weight)
-print(np.isfortran(gpt2_blank.model.attn_c_proj_weight[0]),np.isfortran(gpt2.model.h[0].attn.c_proj.weight))
+    gpt2_blank.model.attn_c_proj_weight.append(model.transformer.h[x].attn.c_proj.weight.detach().cpu().numpy().astype(np.float32))
 
 print("converting attn_c_attn.bias")
 gpt2_blank.model.attn_c_attn_bias = []
 for x in range(12):
-    gpt2_blank.model.attn_c_attn_bias.append(np.zeros(model.transformer.h[x].attn.c_attn.bias.size()).astype(np.float32))
-    convert_1d(model.transformer.h[x].attn.c_attn.bias,gpt2_blank.model.attn_c_attn_bias[x])
-    np.testing.assert_allclose(gpt2_blank.model.attn_c_attn_bias[x],gpt2.model.h[x].attn.c_attn.bias)
-print(np.isfortran(gpt2_blank.model.attn_c_attn_bias[0]),np.isfortran(gpt2.model.h[0].attn.c_attn.bias))  
+    gpt2_blank.model.attn_c_attn_bias.append(model.transformer.h[x].attn.c_attn.bias.detach().cpu().numpy().astype(np.float32))
 
 print("converting attn_c_attn.weight")
 gpt2_blank.model.attn_c_attn_weight = []
 for x in range(12):
-    gpt2_blank.model.attn_c_attn_weight.append(np.zeros(model.transformer.h[x].attn.c_attn.weight.size()).astype(np.float32))
-    convert_2d(model.transformer.h[x].attn.c_attn.weight,gpt2_blank.model.attn_c_attn_weight[x])
-    np.testing.assert_allclose(gpt2_blank.model.attn_c_attn_weight[x],gpt2.model.h[x].attn.c_attn.weight)
-print(np.isfortran(gpt2_blank.model.attn_c_attn_weight[0]),np.isfortran(gpt2.model.h[0].attn.c_attn.weight))   
+    gpt2_blank.model.attn_c_attn_weight.append(model.transformer.h[x].attn.c_attn.weight.detach().cpu().numpy().astype(np.float32))
 
 print("converting ln_1.bias")
 gpt2_blank.model.ln_1_bias = []
 for x in range(12):
-    gpt2_blank.model.ln_1_bias.append(np.zeros(model.transformer.h[x].ln_1.bias.size()).astype(np.float32))
-    convert_1d(model.transformer.h[x].ln_1.bias,gpt2_blank.model.ln_1_bias[x])
-    np.testing.assert_allclose(gpt2_blank.model.ln_1_bias[x],gpt2.model.h[x].ln_1.bias)
-print(np.isfortran(gpt2_blank.model.ln_1_bias[0]),np.isfortran(gpt2.model.h[0].ln_1.bias))   
+    gpt2_blank.model.ln_1_bias.append(model.transformer.h[x].ln_1.bias.detach().cpu().numpy().astype(np.float32))
 
 print("converting ln_1.weight")
 gpt2_blank.model.ln_1_weight = []
 for x in range(12):
-    gpt2_blank.model.ln_1_weight.append(np.zeros(model.transformer.h[x].ln_1.weight.size()).astype(np.float32))
-    convert_1d(model.transformer.h[x].ln_1.weight,gpt2_blank.model.ln_1_weight[x])
-    np.testing.assert_allclose(gpt2_blank.model.ln_1_weight[x],gpt2.model.h[x].ln_1.weight)
-print(np.isfortran(gpt2_blank.model.ln_1_weight[0]),np.isfortran(gpt2.model.h[0].ln_1.weight)) 
+    gpt2_blank.model.ln_1_weight.append(model.transformer.h[x].ln_1.weight.detach().cpu().numpy().astype(np.float32))
 
 print("converting ln_2.bias")
 gpt2_blank.model.ln_2_bias = []
 for x in range(12):
-    gpt2_blank.model.ln_2_bias.append(np.zeros(model.transformer.h[x].ln_2.bias.size()).astype(np.float32))
-    convert_1d(model.transformer.h[x].ln_2.bias,gpt2_blank.model.ln_2_bias[x])
-    np.testing.assert_allclose(gpt2_blank.model.ln_2_bias[x],gpt2.model.h[x].ln_2.bias)
-print(np.isfortran(gpt2_blank.model.ln_2_bias[0]),np.isfortran(gpt2.model.h[0].ln_2.bias))  
+    gpt2_blank.model.ln_2_bias.append(model.transformer.h[x].ln_2.bias.detach().cpu().numpy().astype(np.float32))
 
 print("converting ln_2.weight")
 gpt2_blank.model.ln_2_weight = []
 for x in range(12):
-    gpt2_blank.model.ln_2_weight.append(np.zeros(model.transformer.h[x].ln_2.weight.size()).astype(np.float32))
-    convert_1d(model.transformer.h[x].ln_2.weight,gpt2_blank.model.ln_2_weight[x])
-    np.testing.assert_allclose(gpt2_blank.model.ln_2_weight[x],gpt2.model.h[x].ln_2.weight)
-print(np.isfortran(gpt2_blank.model.ln_2_weight[0]),np.isfortran(gpt2.model.h[0].ln_2.weight))  
+    gpt2_blank.model.ln_2_weight.append(model.transformer.h[x].ln_2.weight.detach().cpu().numpy().astype(np.float32))
    
 
 print("converting wte.weight")
-gpt2_blank.model.wte_weight = np.zeros(model.transformer.wte.weight.size()).astype(np.float32)
-convert_2d(model.transformer.wte.weight,gpt2_blank.model.wte_weight)
-np.testing.assert_allclose(gpt2_blank.model.wte_weight,gpt2.model.wte.weight)
-print(np.isfortran(gpt2_blank.model.wte_weight),np.isfortran(gpt2.model.wte.weight))  
+gpt2_blank.model.wte_weight = model.transformer.wte.weight.detach().cpu().numpy().astype(np.float32)
 
 
 print("converting lm_head.weight")
 print(model.lm_head.weight)
-gpt2_blank.model.lm_head_weight = np.zeros(model.lm_head.weight.size()).astype(np.float32)
-convert_2d(model.lm_head.weight,gpt2_blank.model.lm_head_weight)
-gpt2_blank.model.lm_head_weight = gpt2_blank.model.lm_head_weight.transpose(1,0)
-np.testing.assert_allclose(gpt2_blank.model.lm_head_weight,gpt2.model.lm_head.weight)
-print(np.isfortran(gpt2_blank.model.lm_head_weight),np.isfortran(gpt2.model.lm_head.weight))  
+gpt2_blank.model.lm_head_weight = model.lm_head.weight.detach().cpu().numpy().astype(np.float32).transpose(1,0)
 
 
-with open('new_converted_model_128.pickle', 'wb') as outp:
+with open('new_converted_model_128_3.pickle', 'wb') as outp:
     pickle.dump(gpt2_blank, outp)
