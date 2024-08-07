d2f464a4a2d1e501a4765e6ad3b5f12607124cb3
Rory Clear <roryclear.rc@gmail.com>
2024-08-07 22:45:50 +0100

    make folder if it doesn't exist

--- test_gpt.py
+++ test_gpt.py
@@ -18,6 +18,8 @@ try:
    d = "Metal"
    folder = "metal/"
    print("Using Metal")
+   if os.path.isdir('metal') == False:
+    os.mkdir('metal')
 except ImportError:
     import pyopencl as cl
     print("Using OpenCL")
